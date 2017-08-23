e3b88f0a1c2033830d79db2c952d7df16fce2b2c
ElenaKsirova <30499327+ElenaKsirova@users.noreply.github.com>
2017-08-23 15:07:35 +0300

    Разгрузила функцию createAds. Вынесла константы в начало файла.

--- js/map.js
+++ js/map.js
@@ -1,5 +1,26 @@
 'use strict';
 
+// в задании тип "дворец" не был указан, хотя слово "дворец"
+// встречается в описаниях - ошибка в задании?
+
+var OFFER_CAPTIONS_TO_TYPES = [
+  ['квартира', 'flat'],
+  ['домик', 'house'],
+  ['бунгало', 'bungalo'],
+  ['дворец', 'palace']
+];
+
+
+var OFFER_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+
 function getRandomFromRange(min, max) {
   return (Math.random() * (max - min) + min);
 }
@@ -35,17 +56,6 @@ function capitalizeFirstLetter(string) {
 }
 
 
-// в задании тип "дворец" не был указан, хотя слово "дворец"
-// встречается в описаниях - ошибка в задании?
-
-var OFFER_CAPTIONS_TO_TYPES = [
-  ['квартира', 'flat'],
-  ['домик', 'house'],
-  ['бунгало', 'bungalo'],
-  ['дворец', 'palace']
-];
-
-
 var getOfferTypeByTitle = function (offerTitle) {
   offerTitle = offerTitle.toLowerCase();
 
@@ -70,132 +80,121 @@ var getOfferTypeCaption = function (offerType) {
 };
 
 
+var generateUserNumStrings = function (userCount) {
+  var numStrings = [];
+
+  for (var i = 0; i < userCount; i++) {
+    numStrings[i] = ((i > 9) ? '' : '0') + (i + 1);
+  }
+
+  return numStrings;
+};
+
+
+// каждый вызов extractRandomUserNumString извлекает выбранный элемент
+// из numStrings, таким образом гарантируя уникальность
+// выбранного номера пользователя
+//
+// функция названа extract (а не get) намеренно, чтобы подчеркнуть,
+// что передаваемый ей массив numStrings модифицируется
+
+var extractRandomUserNumString = function (numStrings) {
+  if (!numStrings.length) {
+    return '';
+  }
+
+  var numStringIndex = Math.floor(Math.random() * numStrings.length);
+
+  var numString = numStrings[numStringIndex];
+
+  numStrings.splice(numStringIndex, 1);
+
+  return numString;
+};
+
+
+// каждый вызов getRandomOfferTitle извлекает из offerTitles
+// один случайный элемент, поэтому offerTitles не константа
+
+var offerTitles = [
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
+  'Неуютное бунгало по колено в воде'
+];
+
+var getRandomOfferTitle = function () {
+  if (!offerTitles.length) {
+    return '';
+  }
+
+  var titleIndex = Math.floor(Math.random() * offerTitles.length);
+
+  var title = offerTitles[titleIndex];
+
+  offerTitles.splice(titleIndex, 1);
+
+  return title;
+};
+
+
+// цена за аренду - случайное число, но я сделала так,
+// чтобы это случайное число выглядело более реалистично;
+// например, вряд ли при цене в несколько сот тысяч рублей кто-то
+// будет указывать стоимость с точностью до рубля
+//
+// для диапазона от 1000 до 10000 цены образуются с точностью до 100 рублей
+// от 10000 до 100000 — с шагом 200 рублей
+// от 100000 до 1 млн — с шагом 1000 рублей
+
+var getRandomOfferPrice = function () {
+  var tooPrecisePrice = Math.round(getRandomFromRange(1000, 1000000));
+  var roundedPrice = tooPrecisePrice;
+
+  if (tooPrecisePrice < 10000) {
+    roundedPrice = Math.floor(tooPrecisePrice / 100) * 100;
+  } else if ((tooPrecisePrice > 10000) && (tooPrecisePrice < 100000)) {
+    roundedPrice = Math.floor(tooPrecisePrice / 200) * 200;
+  } else { // (100000 < tooPrecisePrice)
+    roundedPrice = Math.floor(tooPrecisePrice / 1000) * 1000;
+  }
+
+  return roundedPrice;
+};
+
+
+// то же самое с гостями: случайное число гостей зависит от типа жилья,
+// и количества комнат. Тип жилья определяет максимальное количество гостей
+// на 1 комнату, из которого выбирается случайное значение, и уже оно умножается
+// на число комнат
+
+var getRandomOfferGuests = function (offerType, offerRooms) {
+  var maxGuestsPerRoom = 4;
+
+  if (offerType === 'flat') {
+    maxGuestsPerRoom = 5;
+  } else if (offerType === 'house') {
+    maxGuestsPerRoom = 10;
+  } else if (offerType === 'bungalo') {
+    maxGuestsPerRoom = 20;
+  } else if (offerType === 'palace') {
+    maxGuestsPerRoom = 50;
+  }
+
+  return Math.round(getRandomFromRange(1, maxGuestsPerRoom)) * offerRooms;
+};
+
+
 function createAds(adCount) {
   var newAds = [];
-
-
-  var generateUserNumStrings = function (userCount) {
-    var numStrings = [];
-
-    for (var i = 0; i < userCount; i++) {
-      numStrings[i] = ((i > 9) ? '' : '0') + (i + 1);
-    }
-
-    return numStrings;
-  };
-
-
-  // можно было воспользоваться IIFE, не вводя generateUserNumStrings,
-  // но, мне кажется, так нагляднее
 
   var userCount = adCount;
 
   var userNumStrings = generateUserNumStrings(userCount);
-
-
-  // каждый вызов getRandomUserNumString извлекает выбранный элемент
-  // из userNumStrings, таким образом гарантируя уникальность
-  // выбранного номера пользователя
-
-  var getRandomUserNumString = function () {
-    if (!userNumStrings.length) {
-      return '';
-    }
-
-    var numStringIndex = Math.floor(Math.random() * userNumStrings.length);
-
-    var numString = userNumStrings[numStringIndex];
-
-    userNumStrings.splice(numStringIndex, 1);
-
-    return numString;
-  };
-
-
-  // каждый вызов getRandomOfferTitle извлекает из offerTitles
-  // один случайный элемент, поэтому offerTitles не константа
-
-  var offerTitles = [
-    'Большая уютная квартира',
-    'Маленькая неуютная квартира',
-    'Огромный прекрасный дворец',
-    'Маленький ужасный дворец',
-    'Красивый гостевой домик',
-    'Некрасивый негостеприимный домик',
-    'Уютное бунгало далеко от моря',
-    'Неуютное бунгало по колено в воде'
-  ];
-
-  var getRandomOfferTitle = function () {
-    if (!offerTitles.length) {
-      return '';
-    }
-
-    var titleIndex = Math.floor(Math.random() * offerTitles.length);
-
-    var title = offerTitles[titleIndex];
-
-    offerTitles.splice(titleIndex, 1);
-
-    return title;
-  };
-
-
-  // цена за аренду - случайное число, но я сделала так,
-  // чтобы это случайное число выглядело более реалистично;
-  // например, вряд ли при цене в несколько сот тысяч рублей кто-то
-  // будет указывать стоимость с точностью до рубля
-  //
-  // для диапазона от 1000 до 10000 цены образуются с точностью до 100 рублей
-  // от 10000 до 100000 — с шагом 200 рублей
-  // от 100000 до 1 млн — с шагом 1000 рублей
-
-  var getRandomOfferPrice = function () {
-    var tooPrecisePrice = Math.round(getRandomFromRange(1000, 1000000));
-    var roundedPrice = tooPrecisePrice;
-
-    if (tooPrecisePrice < 10000) {
-      roundedPrice = Math.floor(tooPrecisePrice / 100) * 100;
-    } else if ((tooPrecisePrice > 10000) && (tooPrecisePrice < 100000)) {
-      roundedPrice = Math.floor(tooPrecisePrice / 200) * 200;
-    } else { // (100000 < tooPrecisePrice)
-      roundedPrice = Math.floor(tooPrecisePrice / 1000) * 1000;
-    }
-
-    return roundedPrice;
-  };
-
-
-  // то же самое с гостями: случайное число гостей зависит от типа жилья,
-  // и количества комнат. Тип жилья определяет максимальное количество гостей
-  // на 1 комнату, из которого выбирается случайное значение, и уже оно умножается
-  // на число комнат
-
-  var getRandomOfferGuests = function (offerType, offerRooms) {
-    var maxGuestsPerRoom = 4;
-
-    if (offerType === 'flat') {
-      maxGuestsPerRoom = 5;
-    } else if (offerType === 'house') {
-      maxGuestsPerRoom = 10;
-    } else if (offerType === 'bungalo') {
-      maxGuestsPerRoom = 20;
-    } else if (offerType === 'palace') {
-      maxGuestsPerRoom = 50;
-    }
-
-    return Math.round(getRandomFromRange(1, maxGuestsPerRoom)) * offerRooms;
-  };
-
-
-  var OFFER_FEATURES = [
-    'wifi',
-    'dishwasher',
-    'parking',
-    'washer',
-    'elevator',
-    'conditioner'
-  ];
 
 
   for (var i = 0; i < adCount; i++) {
@@ -210,7 +209,7 @@ function createAds(adCount) {
 
     newAds[i] = {
       'author': {
-        'avatar': 'img/avatars/user' + getRandomUserNumString() + '.png'
+        'avatar': 'img/avatars/user' + extractRandomUserNumString(userNumStrings) + '.png'
       },
 
       'offer': {
