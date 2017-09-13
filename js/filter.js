5286acd3a1f1831ed60ed395c5aebce29d2dc9c1
ElenaKsirova <30499327+ElenaKsirova@users.noreply.github.com>
2017-09-13 22:34:45 +0300

    Исправила замечания ESLint

--- js/filter.js
+++ js/filter.js
@@ -1,12 +1,13 @@
 'use strict';
 
 window.filter = (function () {
+  /*
   var filters = document.querySelector('.tokyo__filters');
 
   var offerType = filters.querySelector('.housing_type');
   var offerPrice = filters.querySelector('.housing_price');
   var roomNumber = filters.querySelector('.housing_room-number');
   var guestsNumber = filters.querySelector('.housing_guests-number');
-
-
+  */
+  return {};
 })();
--- js/map.js
+++ js/map.js
@@ -2,75 +2,75 @@
 
 window.map = (function () {
   window.backend.load(
-    function (ads) {
-      var pinElements = window.pin.createPinElements(ads);
+      function (ads) {
+        var pinElements = window.pin.createPinElements(ads);
 
-      var mainPinElement = document.querySelector('.pin__main');
+        var mainPinElement = document.querySelector('.pin__main');
 
-      var pinMap = document.querySelector('.tokyo__pin-map');
+        var pinMap = document.querySelector('.tokyo__pin-map');
 
-      window.utils.addElementsToHTML(pinElements, pinMap);
+        window.utils.addElementsToHTML(pinElements, pinMap);
 
-      window.showCard(ads, pinElements);
+        window.showCard(ads, pinElements);
 
-      var locationLimits = window.data.locationLimits;
+        var locationLimits = window.data.locationLimits;
 
-      var pinCoordsLimits = {
-        min: window.pin.getPinCoordsByLocation({x: locationLimits.x.min, y: locationLimits.y.min}),
-        max: window.pin.getPinCoordsByLocation({x: locationLimits.x.max, y: locationLimits.y.max})
-      };
-
-
-      mainPinElement.addEventListener('mousedown', function (evt) {
-        evt.preventDefault();
-
-        var startCoords = {
-          x: evt.clientX,
-          y: evt.clientY
+        var pinCoordsLimits = {
+          min: window.pin.getPinCoordsByLocation({x: locationLimits.x.min, y: locationLimits.y.min}),
+          max: window.pin.getPinCoordsByLocation({x: locationLimits.x.max, y: locationLimits.y.max})
         };
 
-        var onMouseMove = function (moveEvt) {
-          moveEvt.preventDefault();
 
-          var shift = {
-            x: startCoords.x - moveEvt.clientX,
-            y: startCoords.y - moveEvt.clientY
+        mainPinElement.addEventListener('mousedown', function (evt) {
+          evt.preventDefault();
+
+          var startCoords = {
+            x: evt.clientX,
+            y: evt.clientY
           };
 
-          startCoords = {
-            x: moveEvt.clientX,
-            y: moveEvt.clientY
+          var onMouseMove = function (moveEvt) {
+            moveEvt.preventDefault();
+
+            var shift = {
+              x: startCoords.x - moveEvt.clientX,
+              y: startCoords.y - moveEvt.clientY
+            };
+
+            startCoords = {
+              x: moveEvt.clientX,
+              y: moveEvt.clientY
+            };
+
+
+            var newLeft = (mainPinElement.offsetLeft - shift.x);
+            newLeft = window.utils.getLimitedValue(newLeft, pinCoordsLimits.min.left, pinCoordsLimits.max.left);
+
+            var newTop = (mainPinElement.offsetTop - shift.y);
+            newTop = window.utils.getLimitedValue(newTop, pinCoordsLimits.min.top, pinCoordsLimits.max.top);
+
+            mainPinElement.style.left = newLeft + 'px';
+            mainPinElement.style.top = newTop + 'px';
+
+
+            var newLocation = window.pin.getLocationByPinCoords(mainPinElement);
+
+            window.form.setAddress('x: ' + newLocation.x + ', y: ' + newLocation.y);
           };
 
 
-          var newLeft = (mainPinElement.offsetLeft - shift.x);
-          newLeft = window.utils.getLimitedValue(newLeft, pinCoordsLimits.min.left, pinCoordsLimits.max.left);
+          var onMouseUp = function (upEvt) {
+            upEvt.preventDefault();
 
-          var newTop = (mainPinElement.offsetTop - shift.y);
-          newTop = window.utils.getLimitedValue(newTop, pinCoordsLimits.min.top, pinCoordsLimits.max.top);
+            document.removeEventListener('mousemove', onMouseMove);
+            document.removeEventListener('mouseup', onMouseUp);
+          };
 
-          mainPinElement.style.left = newLeft + 'px';
-          mainPinElement.style.top = newTop + 'px';
-
-
-          var newLocation = window.pin.getLocationByPinCoords(mainPinElement);
-
-          window.form.setAddress('x: ' + newLocation.x + ', y: ' + newLocation.y);
-        };
-
-
-        var onMouseUp = function (upEvt) {
-          upEvt.preventDefault();
-
-          document.removeEventListener('mousemove', onMouseMove);
-          document.removeEventListener('mouseup', onMouseUp);
-        };
-
-        document.addEventListener('mousemove', onMouseMove);
-        document.addEventListener('mouseup', onMouseUp);
-      });
-    },
-    window.error.errorHandler
+          document.addEventListener('mousemove', onMouseMove);
+          document.addEventListener('mouseup', onMouseUp);
+        });
+      },
+      window.error.errorHandler
   );
 
   return {};
